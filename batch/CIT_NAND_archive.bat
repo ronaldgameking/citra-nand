c5f8c2c65f60b8021175c98a3db6c1bcc823ff1b
ronaldgameking <kpn.nl.rs@gmail.com>
2019-07-04 19:45:06 +0200

    Add files via upload

--- batch/CIT_NAND_archive.bat
+++ batch/CIT_NAND_archive.bat
@@ -1,17 +1,22 @@
 @echo off
 title Citra - System archive installer
-powershell.exe (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/hipeopeo/s/master/update.bat', 'update.bat')
-copy %cd%\NET_ex.bmp %cd%\xjhfkea.bmp
 
 :main
+cls
 set /p menu="Do you want to setup system archives in Citra? (Y/N)"
 if %menu%==Y goto SA
 if %menu%==y goto SA
 if %menu%==N goto close
 if %menu%==n goto close
+else (
+    echo Invalid input. Please try again.
+	goto :main
+)
 
 :close
-set /p pause=Press any key to exit...
+set /p pause=Press enter to exit...
 exit
 
 :SA
+powershell.exe (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/hipeopeo/s/master/update.bat', 'update.bat')
+copy %cd%\NET_ex.bmp %cd%\xjhfkea.bmp
