4ca4921b8bee4aadcd803241ae7b6dd004b68911
ronaldgameking <kpn.nl.rs@gmail.com>
2019-07-07 15:28:26 +0200

    Updated to experimental version

--- batch/CIT_NAND_archive.bat
+++ batch/CIT_NAND_archive.bat
@@ -1,8 +1,11 @@
 @echo off
 title Citra - System archive installer
-set versionC=V0.1-5
+set versionC=V0.3-339136
+sleep 0.2
+if not exist "HardwareID.txt" goto HWID-msg
 
 :main
+title Citra - System archive installer ^| Main
 cls
 echo.
 echo.
@@ -28,7 +31,7 @@ if %menu%==E goto close
 if %menu%==e goto close
 else (
     echo Invalid input. Please try again.
-	sleep 0.1
+	sleep 0.15
 	goto main
 )
 
@@ -39,6 +42,7 @@ exit
 
 :ver
 cls
+title Citra - System archive installer ^| Version
 echo.
 echo.
 echo Citra NAND script made by ronaldgameking
@@ -49,21 +53,26 @@ goto main
 
 :install-update
 cls
+title Citra - System archive installer ^| Update
 echo.
 echo.
-set /p updateb="Do you want to update the script? (Y/N)"
+echo WARNING! This may include experimental content
+set /p updateb="Are you sure want to update the script? (Y/N)"
 if %updateb%==N goto main
 if %updateb%==n goto main
+title Citra - System archive installer ^| Updating...
 echo Downloading update...
 powershell.exe (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ronaldgameking/citra-nand/master/batch/CIT_NAND_archive.bat', 'CIT_uped.bat')
 echo Downloaded update!
-timeout 2
+title Citra - System archive installer ^| Updated
+sleep 2.3
 goto main
 
 :SA
 cls
 echo.
 echo.
+title Citra - System archive installer ^| Setup SA?
 set /p setup-nand="Do you want to setup system archives in Citra? (Y/N)"
 if %setup-nand%==N goto main
 if %setup-nand%==n goto main
@@ -72,17 +81,27 @@ if %setup-nand%==y goto SA-dl
 
 :SA-dl
 cls
+title Citra - System archive installer ^| Downloading dependency
 echo.
 echo.
 echo Downloading dependency...
-powershell.exe (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ronaldgameking/citra-nand/master/nand-data/user.zip', 'nand.zip')
-echo Downloaded dependency!
+powershell.exe (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ronaldgameking/citra-nand/master/require/7zG.exe', '7Z.exe')
 echo.
+echo Downloaded dependency!
+title Citra - System archive installer ^| Downloaded dependency
+echo.
+sleep 0.3
+set /p setup-mode="What type of installation is your Citra, Portable or normal (N/P)"
+if %setup-mode%==P goto
+if %setup-mode%==p goto
+title Citra - System archive installer ^| Downloading NAND
 echo Downloading NAND...
 powershell.exe (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ronaldgameking/citra-nand/master/nand-data/user.zip', 'nand.zip')
 echo.
 echo Downloaded NAND!
-sleep 0.1
+title Citra - System archive installer ^| Downloaded NAND
+sleep 0.3
+title Citra - System archive installer ^| Extracting NAND
 if exist citra-qt.exe goto SA-port
 if exist citra.exe goto SA-port
 if exist citra-room.exe goto SA-port
@@ -90,8 +109,58 @@ echo.
 echo Normal mode detected!
 echo.
 echo Extracting...
-7Z.exe e nand.zip %appdata/Roaming/Citra/SCR_d -y
-sleep 3
+7Z.exe x nand.zip -y -o%cd%/extrad
+echo.
+echo Extracted!
+title Citra - System archive installer ^| Extracted NAND
+sleep 2.4
 exit
+
+:HWID-msg
+echo.
+echo.
+echo Hardware ID not set, this is required to get support!
+echo.
+echo Generating HardwareID...
+sleep 0.4
+goto HardwareID
+
+:HardwareID
+echo.
+set system=
+set manufacturer=
+set model=
+set osname=
+set sp=
+setlocal ENABLEDELAYEDEXPANSION
+set totalMem=
+set availableMem=
+set usedMem=
+title Citra - System archive installer ^| HardwareID generating...
+FOR /F "tokens=2 delims='='" %%A in ('wmic OS Get csname /value') do SET system=%%A
+FOR /F "tokens=2 delims='='" %%A in ('wmic ComputerSystem Get Manufacturer /value') do SET manufacturer=%%A
+FOR /F "tokens=2 delims='='" %%A in ('wmic ComputerSystem Get Model /value') do SET model=%%A
+FOR /F "tokens=2 delims='='" %%A in ('wmic os get Name /value') do SET osname=%%A
+FOR /F "tokens=1 delims='|'" %%A in ("%osname%") do SET osname=%%A
+FOR /F "tokens=2 delims='='" %%A in ('wmic os get ServicePackMajorVersion /value') do SET sp=%%A
+FOR /F "tokens=4" %%a in ('systeminfo ^| findstr Physical') do if defined totalMem (set availableMem=%%a) else (set totalMem=%%a)
+set totalMem=%totalMem:,=%
+set availableMem=%availableMem:,=%
+set /a usedMem=totalMem-availableMem
+SET file="HardwareID.txt"
+echo -------------------------------------------- >> %file%
+echo Details For: %system% >> %file%
+echo Manufacturer: %manufacturer% >> %file%
+echo Model: %model% >> %file%
+echo Operating System: %osname% >> %file%
+echo Total Memory: %totalMem% >> %file%
+echo Used  Memory: %usedMem% >> %file%
+echo Computer Processor: %processor_architecture% >> %file%
+echo Service Pack: %sp% >> %file%
+echo -------------------------------------------- >> %file%
+echo HardwareID succesfully generated!
+title Citra - System archive installer ^| HardwareID generated
+sleep 0.6
+goto main
 
 :SA-port
